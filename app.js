78422470543301dbc57780b9ae8f8bb7f2cc66b1
Gbsyi <gbsyi@yandex.ru>
2021-11-14 15:43:44 +0700

    Authentification finished

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose")
+
+const app = express();
+
+app.use(express.json({ extended: true }));
+
+const PORT = config.get('port') || 8080;
+
+app.use("/api/auth",require("./routes/auth"));
+
+async function start(){
+    try{
+        await mongoose.connect(config.get("mongo-uri"),{
+ 
+        });
+        app.listen(PORT,() => {
+            console.log(`App has been started on port ${PORT}.`);
+        }); 
+    }
+    catch (e){
+        console.log("Server error:", e.message);
+        process.exit(1);
+    }
+
+}
+
+start();
